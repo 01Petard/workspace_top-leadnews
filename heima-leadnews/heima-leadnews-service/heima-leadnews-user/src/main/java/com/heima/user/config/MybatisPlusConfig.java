3dc3d9d36fbed4395d6fe4d1a704b4c0213ec10b
01Petard <1520394133@qq.com>
2023-12-18 00:09:13 +0800

    2023.12.17:修改MybatisPlus插件注入方法，从启动配置类改为单独写成一个config文件

---
+++ heima-leadnews/heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/config/MybatisPlusConfig.java
@@ -0,0 +1,31 @@
+package com.heima.user.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration //配置类
+@EnableTransactionManagement //自动管理事务
+@MapperScan("com.heima.user.mapper")
+public class MybatisPlusConfig {
+    //注册插件
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //乐观锁
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        //防止全表更新与删除
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+        //分页插件（最后添加）
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
+
